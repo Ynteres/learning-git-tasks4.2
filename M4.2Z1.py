58e102b30133a2d68850bd4fb23b746178d55b26
Ynteres <69864032+Ynteres@users.noreply.github.com>
2020-08-27 14:04:48 +0200

    approved

--- M4.2Z1.py
+++ M4.2Z1.py
@@ -1,1 +1,13 @@
 # 4.2 Argumenty funkcji
+'''
+def count_them_all(*args):
+    positional_args_count = len(args)
+    print(f"I have received {positional_args_count} positional arguments")
+'''
+def count_them_all(*,a,b):
+    a=0
+    b=0
+    positional_args_count = len(args)
+    print(f"I have received {positional_args_count} positional arguments")
+
+count_them_all(1, 2, 3, "A", a=1, b=2)
---
+++ z4.2 palindromy.py
@@ -0,0 +1,3 @@
+# zadanie z funkcją sprawdzajacą czy dany wyraz jest palindromem a funkcja zwraca prawdę lub fałsz
+
+ 

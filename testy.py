2d46261006ef8d78cfb4e731b48f07c68b1ba059
Ynteres <69864032+Ynteres@users.noreply.github.com>
2020-08-28 20:11:40 +0200

    testy

---
+++ testy.py
@@ -0,0 +1,58 @@
+'''
+Po uruchomieniu programu jesteśmy pytani o typ obliczenia
+
+>> Podaj działanie, posługując się odpowiednią liczbą: 1 Dodawanie, 2 Odejmowanie, 3 Mnożenie, 4 Dzielenie:
+Następnie pobieramy dwie wartości liczbowe.
+
+Korzystając z biblioteki logging, informujemy użytkownika, jakie działanie wykonamy i jakie będą jego argumenty 
+(np. Dodaję 1 i 3).
+
+Następnie wykonujemy obliczenie i drukujemy rezultat z print.
+
+Do pobierania wartości użyj input. Nie ma potrzeby sprawdzania, czy podane argumenty są liczbami, 
+przewidujemy poprawne uzupełnienie.
+'''
+import logging
+logging.basicConfig(level=logging.DEBUG, format='%(asctime)s %(message)s', filename="z4.4logfile.log")
+
+# Cztery funkcje -> add, subtract, multipy i devide  
+
+def add(a, b): 
+    return a + b 
+  
+def subtract(a, b): 
+    return a - b 
+  
+def multiply(a, b): 
+    return a * b 
+  
+def divide(a, b): 
+    return a / b 
+  
+print("Ktora operacje wybierasz -\n"
+    "1. Dodawanie\n"
+    "2. Odejmowanie\n"
+    "3. Mnożenie\n"
+    "4. Dzielenie\n") 
+  
+  
+# odczyt wyboru dokonanego przez operatora  
+option = int(input("Wybierz operacje 1, 2, 3 lub 4 :")) 
+  
+x = float(input("Podaj pierwszą liczbę: ")) 
+y = float(input("Podaj drugą liczbę: ")) 
+  
+if option == 1: 
+    print("Dodaję ",x, "+", y, "=", add(x, y)) 
+  
+elif option == 2: 
+    print("Odejmuję ",x, "-", y, "=", subtract(x, y)) 
+  
+elif option == 3: 
+    print("Mnożę ",x, "*", y, "=", multiply(x, y)) 
+  
+elif option == 4: 
+    print("Dzielę",x, "/", y, "=", divide(x, y)) 
+else: 
+    print("Nie dokonałeś prawidłowego wyboru") 
+
